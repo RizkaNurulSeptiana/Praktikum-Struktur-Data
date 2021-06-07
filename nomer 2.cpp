7d2cc1c04f6edfb79fadc76818156939503f53c0
RizkaNurulSeptiana <72617143+RizkaNurulSeptiana@users.noreply.github.com>
2021-06-07 23:38:48 +0700

    Praktikum 12

--- nomer 2.cpp
+++ nomer 2.cpp
@@ -1,130 +1,106 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <conio.h>
-#define MAX 5
-typedef int Element;
-typedef struct{
-int head;
-int tail;
-Element data[MAX];
-}Queue;
-void initQueue(Queue *q)
-{
-(*q).head=(*q).tail=-1;
-}
-bool isEmpty(Queue q)
-{
-if(q.head==-1 && q.tail==-1)
-return true;
-else
-return false;
-}
-bool isFull(Queue q)
-{
-if((q.head<q.tail && (q.tail-q.head)==MAX-1) ||
-(q.head>q.tail && q.head-q.tail==1))
-return true;
-else
-return false;
-}
-bool isOneElement(Queue q)
-{
-if(q.head==q.tail && q.head!=-1)
-return true;
-else
-return false;
-}
-void Enqueue(Queue *q, Element data)
-{
-if(isFull(*q))
-printf("Queue is Full!");
-else{
-if(isEmpty(*q)){
-(*q).head=(*q).tail=0;
-(*q).data[(*q).tail]=data;
-}else{
-if((*q).tail==MAX-1){
-(*q).tail=0;
-}else{
-(*q).tail++;
-}
-(*q).data[(*q).tail]=data;
-}
-}
-}
-void Dequeue(Queue *q)
-{
-Element temp=(*q).data[(*q).head];
-if(isEmpty(*q))
-printf("Queue is empty!");
-else{
-if(isOneElement(*q)){
-initQueue(q);
-}else{
-if((*q).head==MAX-1)
-(*q).head=0;
-else
-(*q).head++;
-}
-printf("Elemen yang dihapus adalah %d",temp);
-}
-}
-void displayQueue(Queue q)
-{
-int i;
-if(isEmpty(q))
-printf("Queue is empty!");
-else{
-if(q.head<=q.tail){
-for(i=q.head;i<=q.tail;i++){
-printf("%d ", q.data[i]);
-}
-}else{
-for(i=q.head;i<=MAX-1;i++){
-printf("%d ", q.data[i]);
-}
-for(i=0;i<=q.tail;i++){
-printf("%d ", q.data[i]);
-}
-}
-}
-}
-int main()
-{
-Queue q;
-Element input, temp;
-initQueue(&q);
-do
-{
-system("cls");
- printf("RIZKA NURUL SEPTIANA HAKIM\n");
- printf("20051397025\n");
- printf("MI_A_2020\n");
-puts("Menu ");
-puts("1. Enqueue Element");
-puts("2. Dequeue Element");
-puts("3. Display Queue");
-puts("Esc. Exit");
-puts("Pilih : ");
-switch(getch())
-{
- case '1' : if(isFull(q))
-printf("Queue is full!");
-else{
-do{
-printf("Input Number of Integer: ");scanf("%d", &input);
-}while(input==0);
-Enqueue(&q, input);
-}
-break;
-case '2' : Dequeue(&q);
-break;
-case '3' : displayQueue(q);
-break;
-}
-}while(getch()!=27);
-getch();
-return 0;
-}
+//Rizka Nurul Septiana Hakim NIM 20051397025//
+#include <iostream> 
+#include <stdlib.h> 
+
+using namespace std; 
+
+class twoStacks { 
+	int* arr; 
+	int size; 
+	int top1, top2; 
+
+public: 
+	// Constructor 
+	twoStacks(int n) 
+	{ 
+		size = n; 
+		arr = new int[n]; 
+		top1 = n / 2 + 1; 
+		top2 = n / 2; 
+	} 
+
+	// Method to push an element x to stack1 
+	void push1(int x) 
+	{ 
+		// There is at least one empty 
+		// space for new element 
+		if (top1 > 0) { 
+			top1--; 
+			arr[top1] = x; 
+		} 
+		else { 
+			cout << "Stack Overflow"
+				<< " By element :" << x << endl; 
+			return; 
+		} 
+	} 
+
+	// Method to push an element 
+	// x to stack2 
+	void push2(int x) 
+	{ 
+
+		// There is at least one empty 
+		// space for new element 
+		if (top2 < size - 1) { 
+			top2++; 
+			arr[top2] = x; 
+		} 
+		else { 
+			cout << "Stack Overflow"
+				<< " By element :" << x << endl; 
+			return; 
+		} 
+	} 
+
+	// Method to pop an element from first stack 
+	int pop1() 
+	{ 
+		if (top1 <= size / 2) { 
+			int x = arr[top1]; 
+			top1++; 
+			return x; 
+		} 
+		else { 
+			cout << "Stack UnderFlow"; 
+			exit(1); 
+		} 
+	} 
+
+	// Method to pop an element 
+	// from second stack 
+	int pop2() 
+	{ 
+		if (top2 >= size / 2 + 1) { 
+			int x = arr[top2]; 
+			top2--; 
+			return x; 
+		} 
+		else { 
+			cout << "Stack UnderFlow"; 
+			exit(1); 
+		} 
+	} 
+}; 
+
+/* Driver program to test twStacks class */
+int main() 
+{ 
+	twoStacks ts(5); 
+	ts.push1(5); 
+	ts.push2(10); 
+	ts.push2(15); 
+	ts.push1(11); 
+	ts.push2(7); 
+	cout<<"RIZKA NURUL SEPTIANA HAKIM \n";
+	cout<<"20051397025 \n";
+	cout<<"MI_A_2020 \n";
+	cout << "Popped element from stack1 is "
+		<< " : " << ts.pop1() 
+		<< endl; 
+	ts.push2(40); 
+	cout << "\nPopped element from stack2 is "
+		<< ": " << ts.pop2() 
+		<< endl; 
+	return 0; 
+} 
